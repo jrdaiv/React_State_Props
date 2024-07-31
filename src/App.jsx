5765b460794c2bc9b3e1a2432a0afb0bde87f7b2
jrdaiv <jrdaiv@gmail.com>
2024-07-30 20:57:13 -0700

    Code Correction

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import UserProfile from './BugFixes'
+
+function App() {
+  
+
+  return (
+    <>
+    <div className="App">
+      <h1>Bug Fix</h1>
+      <UserProfile />
+    </div>
+    </>
+  )
+}
+
+export default App
